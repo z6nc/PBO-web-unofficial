6022229aeba34e19f9cdb6fdfd9355933d6ad0ca
z6nc <andresvelasquezarotinco@gmail.com>
2025-06-13 18:25:04 -0500

    chore : mantenimiento a las estilos

--- src/components/layout/Header/auidoPlayer.tsx
+++ src/components/layout/Header/auidoPlayer.tsx
@@ -7,17 +7,19 @@ export const AudioPlayer = () => {
     return (
         <div className="w-full  flex flex-col lg:flex-row    items-center  gap-9 bg-transparent border backdrop-blur-lg rounded-lg text-white p-4   md:max-w-xl  ">
             <audio ref={audioRef} src="http://stream.radiojar.com/2fse67zuv8hvv" preload="auto" />
-            <ButtomCustom onclick={togglePlay} estilo="w-20 h-20 lg:w-18 lg:h-18  flex  items-center justify-center bg-white  p-2  rounded-full   hover:scale-105 transition cursor-pointer">
+            <ButtomCustom onclick={togglePlay} estilo="w-20 h-20 lg:w-20 lg:h-20  flex  items-center justify-center bg-white  p-2  rounded-full   hover:scale-105 transition cursor-pointer">
                 {isPlaying ? <PauseIcon /> : <PlayIcon />}
             </ButtomCustom>
             <div className="flex flex-1 items-center gap-4  ">
-                <div className=" w-12  lg:w-16 rounded-lg">
-                    <img className="w-full h-full object-cover rounded-lg  " src="https://pbo.pe/wp-content/uploads/2018/01/marisjoo-760x741.png" alt="" />
-                </div>
-                <div className="flex flex-col text-start">
-                    <span className="font-semibold">PBO Marycarmen Sjoo</span>
-                    <span className="text-sm">Escuchando en vivo</span>
-                </div>
+                {/* este componente de imagen se debe reutilizar y usar logica para las imagenes con referencia a los horarios  */}
+                    <div className=" w-12  lg:w-16 rounded-lg">
+                        <img className="w-full h-full object-cover rounded-lg  " src="https://pbo.pe/wp-content/uploads/2018/01/marisjoo-760x741.png" alt="" />
+                    </div>
+                    <div className="flex flex-col text-start">
+                        <span className="font-semibold">PBO Marycarmen Sjoo</span>
+                        <span className="text-sm">Escuchando en vivo</span>
+                    </div>
+                {/* ------------------------------ */}
 
             </div>
 
