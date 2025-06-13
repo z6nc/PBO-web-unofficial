0c5f958f9dec1f714d899e9c0ac33398adb18de7
z6nc <andresvelasquezarotinco@gmail.com>
2025-06-13 18:25:31 -0500

    update : se agrego el componente listaCanales

--- src/components/layout/Header/heroSection.tsx
+++ src/components/layout/Header/heroSection.tsx
@@ -1,14 +1,16 @@
 import { Title } from "../../common/titleCustom";
 import { AudioPlayer } from "./auidoPlayer";
-// import { RedesSociales } from "../../common/RedesSociales/listRedsocial";
-
+import { ListaCanalesPbo } from "../../common/canales/ListaCanalesPBO";
 export function HeroSection() {
     return (
-        <section className=" flex flex-col justify-center items-center gap-y-2 text-center pt-24 ">
-            <Title title="LA RADIO CON FE" />
-            <p className="text-lg md:text-xl text-white mx-4">PBO es el primer canal multi-plataforma de Perú</p>
-            <AudioPlayer/>
-            {/* <RedesSociales estilo="p-3 border-PBO "/> */}
+        <section className=" flex flex-col justify-around h-full items-center  text-center   ">
+            <div className=" flex  flex-col justify-center items-center">
+                <Title title="LA RADIO CON FE" />
+                <p className="text-lg md:text-xl  text-gray-100 mx-4 pt-2 pb-4"><span className="font-Monserat font-bold">PBO</span> es el primer canal multi-plataforma del Perú</p>
+                <AudioPlayer />
+            </div>
+
+            <ListaCanalesPbo />
         </section>
     )
 
