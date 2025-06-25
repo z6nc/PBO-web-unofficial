5238ea78f9c857b0ef0b7a66e23a2065415ba61a
z6nc <andresvelasquezarotinco@gmail.com>
2025-06-25 10:54:17 -0500

    refactor : reorganize imports and structure in App component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,20 @@
 import { Navbar } from "../components/layout/Navbar/navbar"
 import { HEADERMAIN , HeroSection} from "../components/layout/Header/index"
+import { About } from "../components/layout/About/about"
+import { GridProgramacion } from "../components/layout/GridProgramaciones/gridProgramacion"
 function App() {
-  
-
   return (
     <>
     <HEADERMAIN imagen="/src/assets/fondoPBO.png">
       <Navbar/>
       <HeroSection/>
     </HEADERMAIN>
-
+    <main className="w-full  lg:max-w-7xl mx-auto ">
+    <GridProgramacion/>
+       <About/>
+    </main>
     </>
   )
 }
-
+//  mañana avanzar con imagen central grande los integrantes de la radio y los programas que se emiten
 export default App
