6cbac9c80f98810d85eca79d0e85ba68ec04d9de
z6nc <andresvelasquezarotinco@gmail.com>
2025-06-25 10:54:02 -0500

    add : create GridProgramacion component with SubtitleCustom

---
+++ src/components/layout/GridProgramaciones/gridProgramacion.tsx
@@ -0,0 +1,12 @@
+import { SubtitleCustom } from "../../common/subtitleCustom"
+export const GridProgramacion = () => {
+return(
+    <section> 
+        <SubtitleCustom titulo="Programación" />
+        <div>
+
+        </div>
+    </section>
+)
+
+}
