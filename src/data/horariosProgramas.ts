418dc67b39dfbfbbf198f579b7bfa28b775a0ace
z6nc <andresvelasquezarotinco@gmail.com>
2025-06-25 10:53:48 -0500

    add : update program schedule for multiple days and adjust time formats

--- src/data/horariosProgramas.ts
+++ src/data/horariosProgramas.ts
@@ -2,27 +2,27 @@ import type { PROPSPROGRAMAS } from "../types/programas.types";
 
 export const HorariosProgramas: PROPSPROGRAMAS[] = [
     {
-        dia: "Jueves",
+        dia: "Lunes",
         programas: [
             {
                 programa: "Baella Talks",
                 inicio: "00:00",
-                fin: "01:29",
+                fin: "1:29",
             },
             {
                 programa: "Agatha Lys en PBO",
-                inicio: "01:30",
-                fin: "01:59",
+                inicio: "1:30",
+                fin: "1:59",
             },
             {
                 programa: "PBO con Chema Salcedo",
-                inicio: "02:00",
-                fin: "05:59",
+                inicio: "2:00",
+                fin: "5:59",
             },
             {
                 programa: "PBO Noticias",
-                inicio: "06:00",
-                fin: "09:59",
+                inicio: "6:00",
+                fin: "9:59",
             },
             {
                 programa: "PBO con Chema Salcedo",
@@ -69,13 +69,336 @@ export const HorariosProgramas: PROPSPROGRAMAS[] = [
                 inicio: "22:00",
                 fin: "23:59",
             }
-
-
-
-
         ]
-    }
-
-
-
+    },
+     {
+        dia: "Martes",
+        programas: [
+            {
+                programa: "PBO Noticias",
+                inicio: "00:00",
+                fin: "1:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "2:00",
+                fin: "5:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "6:00",
+                fin: "9:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "10:00",
+                fin: "13:59",
+            },
+            {
+                programa: "PBO Marycarmen Sjoo",
+                inicio: "14:00",
+                fin: "15:59",
+            },
+            {
+                programa: "PBO Salud",
+                inicio: "16:00",
+                fin: "17:59",
+            },
+            {
+                programa: "PBO Campeonisimo",
+                inicio: "18:00",
+                fin: "19:29",
+            },
+            {
+                programa: "PBO Edicion Estelar",
+                inicio: "19:30",
+                fin: "19:59",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "20:00",
+                fin: "20:29",
+            },
+            {
+                programa: "PBO con Rvk y Carmen",
+                inicio: "20:30",
+                fin: "21:29",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "21:30",
+                fin: "21:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "22:00",
+                fin: "23:59",
+            }
+        ]
+    },
+     {
+        dia: "Miercoles",
+        programas: [
+            // {
+            //     programa: "PBO Noticias",
+            //     inicio: "0:00",
+            //     fin: "1:59",
+            // },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "2:00",
+                fin: "5:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "6:00",
+                fin: "9:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "10:00",
+                fin: "13:59",
+            },
+            {
+                programa: "PBO Marycarmen Sjoo",
+                inicio: "14:00",
+                fin: "15:59",
+            },
+            {
+                programa: "PBO Salud",
+                inicio: "16:00",
+                fin: "17:59",
+            },
+            {
+                programa: "PBO Campeonisimo",
+                inicio: "18:00",
+                fin: "19:29",
+            },
+            {
+                programa: "PBO Edicion Estelar",
+                inicio: "19:30",
+                fin: "19:59",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "20:00",
+                fin: "20:29",
+            },
+            {
+                programa: "PBO con Rvk y Carmen",
+                inicio: "20:30",
+                fin: "21:29",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "21:30",
+                fin: "21:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "22:00",
+                fin: "23:59",
+            }
+        ]
+    },
+         {
+        dia: "Jueves",
+        programas: [
+            {
+                programa: "PBO Noticias",
+                inicio: "00:00",
+                fin: "1:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "2:00",
+                fin: "5:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "6:00",
+                fin: "9:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "10:00",
+                fin: "13:59",
+            },
+            {
+                programa: "PBO Marycarmen Sjoo",
+                inicio: "14:00",
+                fin: "15:59",
+            },
+            {
+                programa: "PBO Salud",
+                inicio: "16:00",
+                fin: "17:59",
+            },
+            {
+                programa: "PBO Campeonisimo",
+                inicio: "18:00",
+                fin: "19:29",
+            },
+            {
+                programa: "PBO Edicion Estelar",
+                inicio: "19:30",
+                fin: "19:59",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "20:00",
+                fin: "20:29",
+            },
+            {
+                programa: "PBO con Rvk y Carmen",
+                inicio: "20:30",
+                fin: "21:29",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "21:30",
+                fin: "21:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "22:00",
+                fin: "23:59",
+            }
+        ]
+    },
+     {
+        dia: "Viernes",
+        programas: [
+            {
+                programa: "PBO Noticias",
+                inicio: "00:00",
+                fin: "1:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "2:00",
+                fin: "5:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "6:00",
+                fin: "9:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "10:00",
+                fin: "13:59",
+            },
+            {
+                programa: "PBO Marycarmen Sjoo",
+                inicio: "14:00",
+                fin: "15:59",
+            },
+            {
+                programa: "PBO Salud",
+                inicio: "16:00",
+                fin: "17:59",
+            },
+            {
+                programa: "PBO Campeonisimo",
+                inicio: "18:00",
+                fin: "19:29",
+            },
+            {
+                programa: "PBO Edicion Estelar",
+                inicio: "19:30",
+                fin: "19:59",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "20:00",
+                fin: "20:29",
+            },
+            {
+                programa: "PBO con Rvk y Carmen",
+                inicio: "20:30",
+                fin: "21:29",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "21:30",
+                fin: "21:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "22:00",
+                fin: "23:59",
+            }
+        ]
+    },
+         {
+        dia: "Sabado",
+        programas: [
+            {
+                programa: "PBO Noticias",
+                inicio: "00:00",
+                fin: "1:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "2:00",
+                fin: "5:59",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "6:00",
+                fin: "9:59",
+            },
+            {
+                programa: "PBO con Chema Salcedo",
+                inicio: "10:00",
+                fin: "13:59",
+            },
+            {
+                programa: "Rumbo Minero",
+                inicio: "14:00",
+                fin: "15:29",
+            },
+            {
+                programa: "PBO UMA Emprendedora",
+                inicio: "15:30",
+                fin: "16:29",
+            },
+            {
+                programa: "Vox Populi",
+                inicio: "16:30",
+                fin: "17:29",
+            },
+            {
+                programa: "PBO Noticias",
+                inicio: "17:30",
+                fin: "18:29",
+            },
+            {
+                programa: "Agatha Lys en PBO",
+                inicio: "18:30",
+                fin: "18:59",
+            },
+            {
+                programa: "Rescatando Valores",
+                inicio: "19:00",
+                fin: "19:29",
+            },
+            {
+                programa: "Conversando con el Perú",
+                inicio: "20:00",
+                fin: "20:59",
+            },
+            {
+                programa: "PBO Salud",
+                inicio: "21:00",
+                fin: "21:59",
+            },
+             {
+                programa: "PBO Noticias",
+                inicio: "22:00",
+                fin: "23:54",
+            }
+        ]
+    },
 ];
