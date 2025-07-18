052dabde2c345aa3e00aa12d40bf9260e350b059
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-18 16:25:59 -0500

    fix : update image URLs in ImagenesProgramas and add ColaboradesFotoPortada export

--- src/data/programasImg.ts
+++ src/data/programasImg.ts
@@ -2,19 +2,26 @@ export const  ImagenesProgramas = {
     "Baella Talks": "https://pbo.pe/wp-content/uploads/2022/12/rvk-y-carmen-855x405-1-700x405.png",
     "Agatha Lys en PBO": "https://pbo.pe/wp-content/uploads/2018/01/AGATHALYS-760x741.png",
     "PBO con Chema Salcedo": "https://pbo.pe/wp-content/uploads/2018/01/CHEMA.png",
-    "PBO Noticias": "https://diarioelnoticiero.com/wp-content/uploads/2025/01/Economia_Web-69-3.jpg",
-    "PBO Marycarmen Sjoo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDzMV4afanVXk3BKTSRCZvetS2zMzra5BSEQ&s",
+    "PBO Noticias": "https://pbo.pe/wp-content/uploads/2017/03/PBO-NOTICIAS.png",
+    "PBO Marycarmen Sjoo": "https://pbo.pe/wp-content/uploads/2018/03/MARYCARMEN-700x512.png",
     "PBO Salud": "https://pbo.pe/wp-content/uploads/2022/12/Guiliana-Saldarriaga-760x741.png",
     "PBO Campeonisimo": "https://pbo.pe/wp-content/uploads/2018/01/tigrillo-760x507.png",
     "PBO Edicion Estelar": "https://pbo.pe/wp-content/uploads/2022/12/KATHERINE-HEIGL-1-760x790.png",
-    "PBO con Rvk y Carmen": "https://pbo.pe/wp-content/uploads/2022/12/RICARDO-VAZQUEZ-KUNZE-1-760x741.png",
+    "PBO con Rvk y Carmen": "https://pbo.pe/wp-content/uploads/2022/12/rvk-y-carmen-855x405-1.png",
     "Rumbo Minero": "https://pbo.pe/wp-content/uploads/2022/12/rumbo-minero-1-760x507.png",
     "PBO UMA Emprendedora": "https://pbo.pe/wp-content/uploads/2022/12/UMA-EMPRENDEDORA-1-760x507.png",
     "Vox Populi": "https://pbo.pe/wp-content/uploads/2022/12/VOX-POPULI-1-760x507.png",
     "Rescatando Valores": "https://pbo.pe/wp-content/uploads/2022/12/RESCATANDO-VALORES-1-760x507.png",
     "En la Mira": "https://pbo.pe/wp-content/uploads/2022/12/EN-LA-MIRA-1-760x507.png",
     "Conversando con el Perú": "https://pbo.pe/wp-content/uploads/2022/12/CONVERSANDO-CON-EL-PERU-1-760x507.png",
-    "PBO con Denis Vargas Marín": "https://pbo.pe/wp-content/uploads/2022/12/DENIS-VARGAS-MARIN-1-760x507.png",
+    "PBO con Denis Vargas Marín": "https://pbo.pe/wp-content/uploads/2022/12/rvk-y-carmen-855x405-1.png",
     "Entre Nos": "https://pbo.pe/wp-content/uploads/2022/12/ENTRE-NOS-1-760x507.png",
     "default": "https://pbo.pe/wp-content/uploads/2022/12/rvk-y-carmen-855x405-1-700x405.png"
+};
+
+
+export const  ColaboradesFotoPortada = {
+    "PBO Salud": "https://pbo.pe/wp-content/uploads/2022/12/Guiliana-Saldarriaga-760x741.png",
+     "PBO con Rvk y Carmen":"https://pbo.pe/wp-content/uploads/2022/12/RICARDO-VAZQUEZ-KUNZE-1-760x741.png",
+     "PBO con Chema Salcedo" :"https://pbo.pe/wp-content/uploads/2018/01/CHEMA-760x741.png",
 };
