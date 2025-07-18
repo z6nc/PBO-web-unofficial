c37745096cf5717f746e201714108d5e735f7942
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-18 16:28:17 -0500

    feat : add Banner component for promotional display

---
+++ src/components/layout/Banner/banner.tsx
@@ -0,0 +1,7 @@
+export const Banner = () =>{
+    return(
+        <div className="w-full h-32 bg-PBO flex flex-col items-center justify-center " >
+            <h4 className="text-white text-center text-4xl font-bold">¡La radio con fe! </h4>
+        </div>
+    )
+}
