b5ec1dfc696a6409274bffc9b5db8126b6c172d7
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-22 11:43:55 -0500

    feat: update Banner component to accept dynamic title prop

--- src/components/layout/Banner/banner.tsx
+++ src/components/layout/Banner/banner.tsx
@@ -1,7 +1,7 @@
-export const Banner = () =>{
+export const Banner = ({titulo}: {titulo: string}) =>{
     return(
         <div className="w-full h-32 bg-PBO flex flex-col items-center justify-center " >
-            <h4 className="text-white text-center text-4xl font-bold">¡La radio con fe! </h4>
+            <h4 className="text-white text-center text-3xl md:text-4xl font-bold font-Monserat">{titulo}</h4>
         </div>
     )
 }
