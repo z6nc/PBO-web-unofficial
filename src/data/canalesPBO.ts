fa4746ff44df73d5f261005f834dc5ac2d0c1a7e
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-22 11:42:37 -0500

    feat: update canalesPBO data structure and add additional channels

--- src/data/canalesPBO.ts
+++ src/data/canalesPBO.ts
@@ -1,37 +1,56 @@
-interface PROPSCANALESPBO {
-    id: number
-    categoria: "radio" | "television"
-    frecuencia: string
-    imagen: string
-    alt :string
-    url: string
-}
+import type { PROPSCANALESPBO } from "../types/canalesPBO.types";
 
 export const canalesPBO: PROPSCANALESPBO[] = [
     {
-        id: 1,
+        Nombre: "Radio PBO",
         categoria: "radio",
-        frecuencia: "91.9 FM ",
+        frecuenciaPrincipal: "91.9 FM ",
         imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Logo_de_Radio_PBO.jpg/330px-Logo_de_Radio_PBO.jpg",
-        alt :"imagen de la Radio PBO",
         url: "/"
     },
     {
-        id: 2,
-        categoria: "television",
-        frecuencia: " 735 HD ",
+        Nombre: "Movistar TV",
+        categoria: "cable",
+        frecuenciaPrincipal: " 735 HD ",
         imagen: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Movistar_TV_logo_2018.png",
-        alt : "Portada Movistar TV",
         url: "https://tv.movistar.com.pe/catalog/replay-15676/pbo-15884?type=l7d&sort=0&lastIndex=30"
     },
     {
-        id: 3,
-        categoria: "television",
-        frecuencia: " 515 HD ",
-        imagen: "https://play-lh.googleusercontent.com/MNLWoQMaCpFZ3Os8xgIdBVA3c41VWA-54wkPyUM5WnWTH0rF1r8DdjT6mG1hpo3e7a4",
-        alt : "Portada Claro TV",
+        Nombre: "Claro TV",
+        categoria: "cable",
+        frecuenciaPrincipal: " 515 HD ",
+        imagen: "https://play-lh.googleusercontent.com/CqSX-GfR-UmyYIuzxVBTv4BtkMqfDb5CPkthB53Eczz62JEVI--n7n5vsZraosbMYNI",
         url: "https://www.claro.com.pe/personas/hogar/tv/canales-digitales/"
-    }
+    },
+    {
+        Nombre: "Win TV",
+        categoria: "cable",
+        frecuenciaPrincipal: "Accede ahora",
+        imagen: "https://play-lh.googleusercontent.com/gdMmUJIelopWX8bQ56By9V6Wqw8BWg9oIBC-ZvVuTVV-HKoREc9z5wOmVTcJW2J1xtPS",
+        url: "https://go.wintv.pe/welcome/login"
+    },
+    {
+        Nombre: "Cable Go",
+        categoria: "cable",
+        frecuenciaPrincipal: "Accede ahora",
+        imagen: "https://play-lh.googleusercontent.com/z4NftkwdHbAQ4cR8Z91ZN0Z0uB4ARcvlGrKNqj1DQDyaZwegJsIHk2NixMZziKEXh7aP=w240-h480-rw",
+        url: "https://cablegotv.com/"
+    },
+    {
+        Nombre: "Cablemas",
+        categoria: "cable",
+        frecuenciaPrincipal: "10 SD",
+        subfrecuencias: [
+            {
+                descripcion: "Tacna",
+                frecuencia: "5 SD"
+            },
+        ],
+        imagen: "https://yt3.googleusercontent.com/wtohe2nBwIVuHJzlcwezNQgRxrLlj8L9DVOS9RFgyNk1ecUr5ZSHvf7tEPN-09DA3SZyooePWw=s900-c-k-c0x00ffffff-no-rj",
+        url: "https://cablegotv.com/"
+    },
+     
+
 
 
 ]
