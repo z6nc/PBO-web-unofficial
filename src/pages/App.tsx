26b61b9238ffe83bd6c43671732b4320b52d91cc
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-23 11:33:48 -0500

    refactor: update Banner component to use children prop for content

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import { HEADERMAIN, HeroSection } from "../components/layout/Header/index"
 import { About } from "../components/layout/About/about"
 import { GridProgramacion, GridCableOperadores } from "../components/layout/GridProgramaciones/index";
 import { Banner } from "../components/layout/Banner/banner"
+import { EmailCustom } from "../components/common/Email/emailCustom";
 function App() {
   return (
     <>
@@ -12,9 +13,11 @@ function App() {
       </HEADERMAIN>
       <main className="w-full  lg:max-w-7xl mx-auto  flex flex-col items-center justify-center gap-y-9">
         <GridProgramacion rol="Conductor" numeroGRID={6} posiciontitulo="mr-auto" nombreTitulo="PROGRAMACION SEMANAL" />
-        <Banner titulo="Escúchanos en el 91.9 FM" />
+        <Banner>
+          <h4 className="text-white text-center text-3xl md:text-4xl font-bold font-Monserat">Escúchanos en el 91.9 FM</h4>
+        </Banner>
         <GridProgramacion rol="Periodista" numeroGRID={6} posiciontitulo="mr-auto" nombreTitulo="NUESTROS PRESENTADORES" />
-        <Banner titulo="Disponibles las 24 horas " />
+        <EmailCustom />
         <GridCableOperadores categoria="cable" />
         <About />
       </main>
