295efe7694ccb2b72d61dc80902204a47fbffe9e
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-23 11:33:41 -0500

    refactor: remove estilo prop from RedesSociales component in MovilNavItem

--- src/components/layout/Navbar/movilNavItem.tsx
+++ src/components/layout/Navbar/movilNavItem.tsx
@@ -4,7 +4,7 @@ export const MovilNavItem = () => {
     return (
         <div className="h-[100dvh] bg-white    ">
             <ListaItems estilo="flex flex-col gap-y-4  p-9 text-black uppercase " />
-            <RedesSociales estilo="p-2 border-gray-700 bg-white/25"/>
+            <RedesSociales />
         </div>
     )
 }
