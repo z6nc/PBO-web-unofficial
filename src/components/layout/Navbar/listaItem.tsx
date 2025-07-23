ca68649bc52888977e44885bdf4b066a721623c7
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-23 11:33:33 -0500

    style: clean up whitespace in ListaItems component

--- src/components/layout/Navbar/listaItem.tsx
+++ src/components/layout/Navbar/listaItem.tsx
@@ -4,15 +4,15 @@ export const ListaItems = ({ estilo }: EstiloProps) => {
 
 
     return (
-        <ul className={`  tracking-wider ${estilo}`}>
+        <ul className={`tracking-wider ${estilo}`}>
             {
                 NavbarItems.map((i, idx) => {
-                         const importante = i.isImportante  ? "font-semibold  bg-red-600 text-white" : ""
+                    const importante = i.isImportante ? "font-semibold  bg-red-600 text-white" : ""
                     return (
                         <li className={`py-1 px-2 cursor-pointer  rounded-lg overflow-hidden ${importante} transition-colors duration-100 ease-linear hover:bg-PBO  `} key={idx}>
                             {i.item}
                         </li>
-                        
+
                     )
                 })
 
