2534f3d571620f1cbf523375999f6a27e8aaf66c
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-23 11:32:44 -0500

    feat: add type prop to ButtomCustom component for button type specification

--- src/components/common/buttomCustom.tsx
+++ src/components/common/buttomCustom.tsx
@@ -2,10 +2,11 @@ import type { EstiloProps } from "../../types/estilo.types"
 interface PROPSBUTTOM extends EstiloProps{
     onclick?: () => void
     children : React.ReactNode 
+    type?: "button" | "submit" 
 }
-export const ButtomCustom = ({onclick , children ,estilo}:PROPSBUTTOM)=>{
+export const ButtomCustom = ({onclick , children ,estilo, type}:PROPSBUTTOM)=>{
     return(
-        <button className={estilo} onClick={onclick} >
+        <button className={`cursor-pointer ${estilo}`} onClick={onclick} type={type}>
             {children}
         </button>
     )
