24ee53dea48f42d63c1688b2d6a2339a80a34120
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-23 11:34:13 -0500

    feat: add SubtitleCustom component for displaying subtitles

---
+++ src/components/common/titulos/subtitleCustom.tsx
@@ -0,0 +1,12 @@
+import type { EstiloProps } from "../../../types/estilo.types";
+
+interface SubtitleCustomProps extends EstiloProps {
+    titulo: string;
+}
+export const SubtitleCustom = ({ titulo , estilo}:SubtitleCustomProps) => {
+    return(
+        <div className={`bg-PBO text-white py-4 px-4  my-7 ${estilo}`}>
+            <h2 className="text-3xl font-bold  font-Monserat ">{titulo}</h2>
+        </div>
+    );
+}
