0c220b8f8e6d6271f75a719b202104edf244895d
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-25 11:56:19 -0500

    feat: add estilo prop to RedesSociales component for customizable styling

--- src/components/common/RedesSociales/listRedsocial.tsx
+++ src/components/common/RedesSociales/listRedsocial.tsx
@@ -1,9 +1,10 @@
 import { RedesSocialesPBO } from "../../../data/redesSociales";
-export function RedesSociales() {
+import type { EstiloProps } from "../../../types/estilo.types";
+export function RedesSociales({estilo}: EstiloProps) {
   return (
-    <ul className="flex justify-center items-center space-x-2 text-white my-3">
+    <ul className={`flex justify-center items-center space-x-2 text-white my-3 ${estilo}`}>
       {RedesSocialesPBO.map((red) => (
-        <li key={red.id} className={" p-2 rounded-lg border-2 bg-gray-800 backdrop-brightness-90  border-PBO"}>
+        <li key={red.id} className={" p-2 rounded-lg border-2 bg-gray-800 backdrop-brightness-90  "}>
           <a href={red.url} target="_blank " >
              {red.logo}
           </a>
