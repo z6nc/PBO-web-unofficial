9c802858780270dc5f63deebe9f0a6abc0886a1d
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-25 11:58:21 -0500

    feat: add missing icon exports to index file

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,5 @@
 export { HamburIcon } from "./HamburIcon";
 export { ExitIcon } from "./exitIcon";
-
+export {PinMap} from "./pinMap";
+export { ShareIcon } from "./shareIcon";
+export { ArrowDownIcon } from "./arrowIcon";
