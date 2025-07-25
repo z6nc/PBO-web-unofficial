7ad4ccaf32ac24dea03a6c6ab6f252baa31ddc6f
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-25 11:55:47 -0500

    feat: add WhatsApp icon and update social media icons list

--- src/data/redesSociales.tsx
+++ src/data/redesSociales.tsx
@@ -1,29 +1,34 @@
-import { TiktokIcon ,FacebookIcon,IstagramIcon,XIcon,YoutubeIcon } from "../components/icons/redesSocialesIcons"
+import { TiktokIcon, FacebookIcon, IstagramIcon, XIcon, YoutubeIcon ,WhatsApp} from "../components/icons/redesSocialesIcons"
 export const RedesSocialesPBO = [
-    {
-        id:"Youtube",
-        url :"https://www.youtube.com/@PBO",
-        logo : <YoutubeIcon/>
-    },
+  {
+    id: "Youtube",
+    url: "https://www.youtube.com/@PBO",
+    logo: <YoutubeIcon />
+  },
+  {
+    id: "Facebook",
+    url: "https://www.facebook.com/PBOPeru",
+    logo: <FacebookIcon />
+  },
+  {
+    id: "X",
+    url: "https://x.com/PBOPeru",
+    logo: <XIcon />
+  },
+  {
+    id: "Instagram",
+    url: "https://www.instagram.com/pbo_digital/#",
+    logo: <IstagramIcon />
+  },
+  {
+    id: "Tik Tok",
+    url: "https://www.tiktok.com/@pbo_digital",
+    logo: <TiktokIcon />
+  },
       {
-        id:"Facebook",
-        url :"https://www.facebook.com/PBOPeru",
-        logo : <FacebookIcon/>
-    },
-      {
-        id:"X",
-        url :"https://x.com/PBOPeru",
-        logo : <XIcon/>
-    },
-      {
-        id:"Instagram",
-        url :"https://www.instagram.com/pbo_digital/#",
-        logo : <IstagramIcon/>
-    },
-     {
-        id:"Tik Tok",
-        url :"https://www.tiktok.com/@pbo_digital",
-        logo : <TiktokIcon/>
-    }
+    id: "whatsapp",
+    url: "https://api.whatsapp.com/send/?phone=5116449550&text&type=phone_number&app_absent=0",
+    logo: <WhatsApp estilo="size-6" />
+  }
 
 ]
