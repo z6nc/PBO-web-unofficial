ba4f6ae535b6ccf0f1f443a3120cacd596d2e60d
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-25 11:56:45 -0500

    feat: add ArrowDownIcon and PinMap components for enhanced iconography

---
+++ src/components/icons/arrowIcon.tsx
@@ -0,0 +1,8 @@
+export const ArrowDownIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4  lg:size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m15 15-6 6m0 0-6-6m6 6V9a6 6 0 0 1 12 0v3" />
+        </svg>
+
+    )
+}
