1cdb7f4a4e3a06f49f72845e34a158f6ef4feb2b
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-25 11:58:06 -0500

    refactor: restructure About component layout for improved readability and organization

--- src/components/layout/About/about.tsx
+++ src/components/layout/About/about.tsx
@@ -2,15 +2,24 @@ import { RedesSociales } from "../../common/RedesSociales/listRedsocial";
 import { SubtitleCustom } from "../../common/titulos/subtitleCustom";
 export const About = () => {
     return (
-        <div className="flex flex-col items-start  p-4 h-[60dvh]">
-            <SubtitleCustom titulo="Acerca de PBO" />
-            <p className="text-lg mb-2">
-                ¡Sintoniza PBO Radio 91.9 FM, la radio con fe! Fundado en 2016 por <span className="font-bold">Phillip Butters</span>, somos la primera multiplataforma del Perú.
-                Noticias al instante, opinión sin filtros, tecnología, salud, deportes y entretenimiento.
-                ¡Conéctate con lo que de verdad importa!
-            </p>
-            <RedesSociales />
-        </div>
+        <section className="grid grid-cols-1 md:grid-cols-3 gap-4   mx-4 lg:mx-0">
+            <article>
+                <SubtitleCustom estilo=" text-center" titulo="Acerca de PBO" />
+                <p className="text-base">
+                    ¡Sintoniza PBO Radio 91.9 FM, la radio con fe! Fundado en 2016 por <span className="font-bold">Phillip Butters</span>, somos la primera multiplataforma del Perú.
+                    Noticias al instante, opinión sin filtros, tecnología, salud, deportes y entretenimiento.
+                    ¡Conéctate con lo que de verdad importa!
+                </p>
+            </article>
+            <article>
+                <SubtitleCustom estilo=" text-center" titulo="Programas de hoy" />
+                <RedesSociales />
+            </article>
+            <article>
+                <SubtitleCustom estilo=" text-center" titulo="Redes sociales" />
+                <RedesSociales />
+            </article>
+        </section>
     );
 }
 
