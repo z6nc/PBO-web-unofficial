3bd57d6e3eccca3088d36ae2da76eb68677e3fd5
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 11:01:17 -0500

    refactor: simplify PROPSINFOPROGRAMA interface by using Pick to extract programa type

--- src/types/infoPrograma.types.ts
+++ src/types/infoPrograma.types.ts
@@ -1,5 +1,7 @@
-export interface PROPSINFOPROGRAMA {
-    programa: string;
+import type { Programa } from "./programas.types";
+type programas = Pick<Programa, 'programa' >;
+
+export interface PROPSINFOPROGRAMA extends programas {
     inicio: string;
     fin: string;
     imagen?: string;
