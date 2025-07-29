974731575b471c93c57a4ae9cd665f36b28871dd
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:20:33 -0500

    fix: update RedesSociales component to improve accessibility and JSX structure

--- src/components/common/RedesSociales/listRedsocial.tsx
+++ src/components/common/RedesSociales/listRedsocial.tsx
@@ -1,11 +1,10 @@
 import { RedesSocialesPBO } from "../../../data/redesSociales";
-import type { EstiloProps } from "../../../types/estilo.types";
-export function RedesSociales({estilo}: EstiloProps) {
+export function RedesSociales({estilo}: {estilo?:string}) {
   return (
-    <ul className={`flex justify-center items-center space-x-2 text-white my-3 ${estilo}`}>
+    <ul aria-label="Redes sociales de PBO" className={`flex justify-center items-center space-x-2 text-white my-3 ${estilo}`}>
       {RedesSocialesPBO.map((red) => (
-        <li key={red.id} className={" p-2 rounded-lg border-2 bg-gray-800 backdrop-brightness-90  "}>
-          <a href={red.url} target="_blank " >
+        <li key={red.NombreRedSocial} className={" p-2 rounded-lg border-2 bg-gray-800 backdrop-brightness-90  "}>
+          <a href={red.url} target="_blank" rel="noopener noreferrer" aria-label={`Visitar ${red.NombreRedSocial} de PBO`}>
              {red.logo}
           </a>
         </li>
