6c3f1ed713a080de00074c1df9bc344e647fdde7
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:21:01 -0500

    fix: update Title component to use 'titulo' prop instead of 'title'

--- src/components/common/titulos/titleCustom.tsx
+++ src/components/common/titulos/titleCustom.tsx
@@ -1,6 +1,5 @@
-import type { PROPSTITLE } from "../../../types/title.types"
-export const Title = ({title}:PROPSTITLE)=>{
+export const Title = ({ titulo }: { titulo: string }) => {
     return (
-        <h1 className="font-Monserat text-center text-white text-4xl md:text-5xl 2xl:text-7xl font-black drop-shadow-md drop-shadow-black">{title}</h1>
+        <h1 className="font-Monserat text-center text-white text-4xl md:text-5xl 2xl:text-7xl font-black drop-shadow-md drop-shadow-black">{titulo}</h1>
     )
 }
