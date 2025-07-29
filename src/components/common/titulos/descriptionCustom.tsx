d0af8caf8c64c941d4b56666552007970c79445c
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:20:40 -0500

    fix: update DescriptionCustom component to use 'descripcion' prop instead of 'title'

--- src/components/common/titulos/descriptionCustom.tsx
+++ src/components/common/titulos/descriptionCustom.tsx
@@ -1,8 +1,6 @@
-import type { PROPSTITLE } from "../../../types/title.types"
-import type { EstiloProps } from "../../../types/estilo.types"
 
-export const DescriptionCustom = ({ title, estilo }: PROPSTITLE & EstiloProps) => {
+export const DescriptionCustom = ({ descripcion , estilo }:{descripcion :string , estilo?:string}) => {
     return (
-        <p className={` text-white text-xl md:text-lg 2xl:text-xl font-bold ${estilo}`}>{title}</p>
+        <p className={` text-white text-xl md:text-lg 2xl:text-xl font-bold ${estilo}`}>{descripcion}</p>
     )
 }
