8bb1a4fb01188a9d18c985cbe7b0542475bd4ca6
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:23:30 -0500

    fix: correct spelling of 'ButtonCustom' in AudioPlayer component

--- src/components/layout/Header/playerAudio/auidoPlayer.tsx
+++ src/components/layout/Header/playerAudio/auidoPlayer.tsx
@@ -1,5 +1,5 @@
 import { useAudioPlayer } from "../../../../hooks/useAuidoPlayer";
-import { ButtomCustom } from "../../../common";
+import { ButtonCustom } from "../../../common";
 import { HostProfile } from "./HostProfile";
 import { PlayIcon, MuteIcon, PauseIcon, VolumenIcon } from "../../../icons/auidoPlayerIcon";
 import { useProgramas } from "../../../../hooks/useProgramas";
@@ -9,13 +9,13 @@ export const AudioPlayer = () => {
     return (
         <div className="w-full  flex flex-col lg:flex-row    items-center  gap-9 bg-transparent border backdrop-blur-lg rounded-lg text-white p-4   md:max-w-xl  ">
             <audio ref={audioRef} src="http://stream.radiojar.com/2fse67zuv8hvv" preload="auto" />
-            <ButtomCustom onclick={togglePlay} estilo="w-20 h-20 lg:w-20 lg:h-20  flex  items-center justify-center bg-white  p-2  rounded-full   hover:scale-105 transition cursor-pointer">
+            <ButtonCustom onClick={togglePlay} ariaLabel={isPlaying ? "Pausar" : "Reproducir"} estilo="w-20 h-20 lg:w-20 lg:h-20  flex  items-center justify-center bg-white  p-2  rounded-full   hover:scale-105 transition cursor-pointer">
                 {isPlaying ? <PauseIcon /> : <PlayIcon />}
-            </ButtomCustom>
+            </ButtonCustom>
             {currentProgram && <HostProfile ActualPrograma={currentProgram} />}
-            <ButtomCustom onclick={toggleMute} estilo="text-gray-300 hover:text-white cursor-pointer">
+            <ButtonCustom onClick={toggleMute} ariaLabel={isMuted || volume === 0 ? "Desmutear" : "Mutear"} estilo="text-gray-300 hover:text-white cursor-pointer">
                 {isMuted || volume === 0 ? <MuteIcon /> : <VolumenIcon />}
-            </ButtomCustom>
+            </ButtonCustom>
 
         </div>
     );
