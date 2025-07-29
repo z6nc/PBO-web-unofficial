4b1b2cb169bd29fced685815392ab734a81e6dfc
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:23:58 -0500

    fix: correct spelling of 'ButtonCustom' and improve formatting in Navbar component

--- src/components/layout/Navbar/navbar.tsx
+++ src/components/layout/Navbar/navbar.tsx
@@ -1,27 +1,25 @@
 import { useState } from "react"
-import { Logo ,ButtomCustom } from "../../common/index"
+import { Logo, ButtonCustom } from "../../common/index"
 import { ListaItems } from "./listaItem"
 import { MovilNavItem } from "./movilNavItem"
-import { HamburIcon , ExitIcon} from "../../icons/index"
-export const Navbar = () =>{
-    const [isOpen , setOpen] = useState<boolean>(false)
-    const openClass = isOpen ? "hidden" :"block"
-    const closeClass = isOpen ? "block" : "hidden"
-    return(
-        <>
-        <nav className="flex relative justify-around py-4  items-center  hover:backdrop-blur-md  bg-PBO lg:bg-transparent  ">
-          <Logo estilo="w-20 lg:w-32 overflow-hidden" />
-             <ListaItems estilo="hidden lg:inline-flex text-white gap-x-9  "/>
-              <ButtomCustom onclick={()=>setOpen(!isOpen)} estilo="block lg:hidden">
-                <HamburIcon estilo={openClass}/>
-                <ExitIcon estilo={closeClass}/>
-              </ButtomCustom>
-            
-           
-        </nav>
-          {
-                isOpen && <MovilNavItem/>
-              }
-              </>
-    )
+import { HamburIcon, ExitIcon } from "../../icons/index"
+export const Navbar = () => {
+  const [isOpen, setOpen] = useState<boolean>(false)
+  const openClass = isOpen ? "hidden" : "block"
+  const closeClass = isOpen ? "block" : "hidden"
+  return (
+    <>
+      <nav className="flex relative justify-around py-4  items-center  hover:backdrop-blur-md  bg-PBO lg:bg-transparent  ">
+        <Logo estilo="w-20 lg:w-32 overflow-hidden" priority={true} />
+        <ListaItems estilo="hidden lg:inline-flex text-white gap-x-9  " />
+        <ButtonCustom onClick={() => setOpen(!isOpen)} estilo="block lg:hidden" ariaLabel="abrir menu movil" type="button">
+          <HamburIcon estilo={openClass} />
+          <ExitIcon estilo={closeClass} />
+        </ButtonCustom>
+      </nav>
+      {
+        isOpen && <MovilNavItem />
+      }
+    </>
+  )
 }
