f70879abb9c047fbb0df84989483b8b4fad6c1d9
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:21:37 -0500

    fix: update Banner component to accept 'estilo' prop for customizable styles

--- src/components/layout/Banner/banner.tsx
+++ src/components/layout/Banner/banner.tsx
@@ -1,9 +1,10 @@
-interface PROPSBANNER {
+interface PROPSBANNER  {
     children: React.ReactNode
+    estilo?: string
 }
-export const Banner = ({ children }: PROPSBANNER) => {
+export const Banner = ({ children , estilo }: PROPSBANNER) => {
     return (
-        <article className="w-full min-h-32 max-h-max bg-PBO flex flex-col items-center justify-center overflow-hidden " >
+        <article className={`w-full min-h-4 max-h-max bg-PBO flex flex-col items-center justify-center overflow-hidden ${estilo}`} >
             {children}
         </article>
     )
