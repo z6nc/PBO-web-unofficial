ad00180bbd6330ce0dab165f7d3857151f948a07
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:23:10 -0500

    fix: enhance HEADERMAIN component by adding priority and info props for improved image loading and accessibility

--- src/components/layout/Header/header.tsx
+++ src/components/layout/Header/header.tsx
@@ -2,17 +2,20 @@
 interface PROPSHEADER {
     children: React.ReactNode
     imagen: string
+    priority?: boolean
+    info:string
 }
-export const HEADERMAIN = ({ children, imagen }: PROPSHEADER) => {
+export const HEADERMAIN = ({ children, imagen, priority , info}: PROPSHEADER) => {
+
     return (
         <header className="relative max-h-max md:h-[100dvh] overflow-hidden w-full  ">
             <img
                 className="absolute inset-0 -z-10 w-full h-full object-cover brightness-[60%]"
                 src={imagen}
-                alt=""
+                alt={`Imagen de fondo de ${info}`}
+                loading={priority ? "eager" : "lazy"}
             />
-
-            {children} 
+            {children}
         </header>
     )
 }
