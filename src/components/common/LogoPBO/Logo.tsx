197af7cbddf797f0ebd1f7b665a40c7203652cbe
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:20:24 -0500

    feat: add Logo component for displaying the PBO logo with customizable styles

---
+++ src/components/common/LogoPBO/Logo.tsx
@@ -0,0 +1,14 @@
+import type  {LogoProps} from "../../../types/logos.types";
+
+export const Logo = ({ estilo, priority=false }: LogoProps) => {
+  return (
+    <div className={estilo}>
+      <img
+        className={`w-full h-full object-cover shadow-2xl`}
+        src="https://pbo.pe/wp-content/uploads/2022/11/pbo.png"
+        alt="Logo de PBO"
+        loading={priority ? "eager" : "lazy"}
+      />
+    </div>
+  );
+};
