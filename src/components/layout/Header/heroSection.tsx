294e8cef0da28698e10290396fc21761427aaf0e
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:23:18 -0500

    fix: correct prop name from 'title' to 'titulo' in HeroSection component

--- src/components/layout/Header/heroSection.tsx
+++ src/components/layout/Header/heroSection.tsx
@@ -5,11 +5,10 @@ export function HeroSection() {
     return (
         <section className=" flex flex-col justify-around h-full items-center  text-center   ">
             <div className=" flex  flex-col justify-center items-center">
-                <Title title="LA RADIO CON FE" />
+                <Title titulo="LA RADIO CON FE" />
                 <p className="text-lg md:text-xl  text-gray-100 mx-4 pt-2 pb-4"><span className="font-Monserat font-bold">PBO</span> es el primer canal multi-plataforma del Perú</p>
                 <AudioPlayer />
             </div>
-
             <ListaCanalesPbo />
         </section>
     )
