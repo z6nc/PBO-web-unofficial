bf101cec8e1ca15bc31c14d85e0b2a94e55a4ace
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:19:52 -0500

    fix: update SectionContactos to use direct data lookup for WhatsApp and improve JSX formatting

--- src/components/common/contactos/contactos.tsx
+++ src/components/common/contactos/contactos.tsx
@@ -1,10 +1,10 @@
 import { PinMap } from "../../icons"
-import { WhatsApp } from "../../icons/redesSocialesIcons"
+import { WhatsApp } from "../../icons/redesSocialesIcons";
 import type { EstiloProps } from "../../../types/estilo.types"
-import { useFindData } from "../../../hooks/useFindData";
 import { RedesSocialesPBO } from "../../../data/redesSociales";
 export function SectionContactos({ estilo }: EstiloProps) {
-    const redesSociales = useFindData(RedesSocialesPBO, "whatsapp");
+    const redesSociales = RedesSocialesPBO.find(item => item.NombreRedSocial === "WhatsApp");
+
     return (
         <div className={`${estilo} flex flex-col gap-y-9 lg:gap-y-5 text-white py-2`}>
             <div className="flex  flex-col text-center  lg:text-start lg:flex-row items-center gap-2">
@@ -14,12 +14,14 @@ export function SectionContactos({ estilo }: EstiloProps) {
                     <span>Av. Angamos Oeste 651, Miraflores, Perú</span>
                 </div>
             </div>
+            {/* puedes reutilizar el componente de enlace */}
             {
                 redesSociales ? (
-                    <a key={redesSociales.id} href={redesSociales.url} target="_blank" rel="noopener noreferrer" className="
+                    <a key={redesSociales.NombreRedSocial} href={redesSociales.url} target="_blank" rel="noopener noreferrer" className="
                      flex  flex-col text-center  lg:text-start lg:flex-row items-center gap-2 pl-1">
-                        <WhatsApp estilo="size-12 lg:size-9" />
-                        <span>+51 1 6449550</span>
+                        <WhatsApp estilo="size-10 lg:size-8" />
+                        <span className="font-bold">{redesSociales.NombreRedSocial}</span>
+                        <span>{redesSociales.info}</span>
                     </a>
                 ) : null
             }
