a0ff945b7df1fb48d8ebcb942e3681c70d4c9025
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:20:08 -0500

    fix: enhance InputCustom component to include error handling and default type

--- src/components/common/Email/inputCustom.tsx
+++ src/components/common/Email/inputCustom.tsx
@@ -1,10 +1,12 @@
-interface InputCustomProps {
-    type?: string;
-    placeholder?: string;
-    required?: boolean;
-}
-export const InputCustom = ({ type, placeholder, required }: InputCustomProps) => {
-    return(
-        <input className=" bg-BlackPBO text-white placeholder:text-white px-4 rounded-md w-96 h-16" type={type} placeholder={placeholder} required={required}  />
+import type { InputCustomProps } from "../../../types/input.types"
+export const InputCustom = ({ type = "text", placeholder , error, ...rest }: InputCustomProps) => {
+    return (
+        <div>
+            <input className="w-72 md:w-96 h-16 bg-BlackPBO text-white placeholder:text-white px-4 rounded-md  focus:outline-none
+             focus:ring-2 focus:ring-white transition-colors" type={type} placeholder={placeholder}  {...rest} />
+            {error && (
+                <span className="text-white text-sm mt-1">{error}</span>
+            )}
+        </div>
     )
 }
