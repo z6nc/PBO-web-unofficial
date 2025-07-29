50bb91b8767e5b4ad6fc048d5df5bab8e8bca76f
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-29 10:16:01 -0500

    feat: wrap App component with BrowserRouter for routing support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-import App from './pages/App.tsx'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
