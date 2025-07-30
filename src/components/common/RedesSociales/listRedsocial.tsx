d2d1aa4b41b2b09e84b48d259289d6219d15dff8
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-30 11:16:37 -0500

    fix: remove extra whitespace in list item class for RedesSociales component

--- src/components/common/RedesSociales/listRedsocial.tsx
+++ src/components/common/RedesSociales/listRedsocial.tsx
@@ -3,7 +3,7 @@ export function RedesSociales({estilo}: {estilo?:string}) {
   return (
     <ul aria-label="Redes sociales de PBO" className={`flex justify-center items-center space-x-2 text-white my-3 ${estilo}`}>
       {RedesSocialesPBO.map((red) => (
-        <li key={red.NombreRedSocial} className={" p-2 rounded-lg border-2 bg-gray-800 backdrop-brightness-90  "}>
+        <li key={red.NombreRedSocial} className={" p-2 rounded-lg border-2 bg-gray-800 backdrop-brightness-90 "}>
           <a href={red.url} target="_blank" rel="noopener noreferrer" aria-label={`Visitar ${red.NombreRedSocial} de PBO`}>
              {red.logo}
           </a>
