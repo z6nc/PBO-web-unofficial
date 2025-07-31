d357d8402a2e9fffe1055a0f297c332c6ada8e28
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:37:23 -0500

    feat: add AlertaInformativa component for displaying informational alerts

---
+++ src/components/common/avisosBanner/alertaInformativa.tsx
@@ -0,0 +1,9 @@
+import { Banner } from "../../layout/Banner/banner"
+
+export const AlertaInformativa = () => {
+    return (
+        <Banner>
+            <p className="text-white py-1">Este no es el sitio oficial de PBO </p>
+        </Banner>
+    )
+}
