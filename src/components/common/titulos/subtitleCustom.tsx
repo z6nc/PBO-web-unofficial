f07966dc6557307f4f72919928c69b9c61c7f1ac
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:37:52 -0500

    feat: make estilo prop optional in SubtitleCustom component

--- src/components/common/titulos/subtitleCustom.tsx
+++ src/components/common/titulos/subtitleCustom.tsx
@@ -1,5 +1,5 @@
 
-export const SubtitleCustom = ({ Subtitulo, estilo }: { Subtitulo: string, estilo: string }) => {
+export const SubtitleCustom = ({ Subtitulo, estilo }: { Subtitulo: string, estilo?: string }) => {
     return (
         <div className={`bg-PBO text-white py-4 px-4  my-7 ${estilo}`}>
             <h2 className="text-3xl font-bold  font-Monserat ">{Subtitulo}</h2>
