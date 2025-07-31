72510064205030157bebbb3f6c43444b7700c227
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:38:27 -0500

    feat: add LayoutPrincipal component for main layout structure

---
+++ src/components/layout/LayoutPrincipal/layoutPrincipal.tsx
@@ -0,0 +1,7 @@
+export function LayoutPrincipal({ children }: { children: React.ReactNode }) {
+    return (
+      <main className="w-full  lg:max-w-7xl mx-auto flex flex-col items-center justify-center gap-y-9 py-4">
+        {children}
+      </main>
+    );
+}
