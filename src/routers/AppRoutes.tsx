f015294e1ae642b461069f0943b3a8a80f34a38b
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:39:30 -0500

    feat: add route for NuestroEquipo page in AppRoutes

--- src/routers/AppRoutes.tsx
+++ src/routers/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
+import NuestroEquipo from "../pages/nuestroEquipo";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/nuestro-equipo" element={<NuestroEquipo />} />
+
     </Routes>
   );
 }
