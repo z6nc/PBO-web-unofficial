af26251f4ad4ac2fdbf96a0f38938d29860ad248
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:38:16 -0500

    style: format code for consistency in HEADERMAIN component

--- src/components/layout/Header/header.tsx
+++ src/components/layout/Header/header.tsx
@@ -5,12 +5,12 @@ interface PROPSHEADER {
     priority?: boolean
     info:string
 }
-export const HEADERMAIN = ({ children, imagen, priority , info}: PROPSHEADER) => {
+export const HEADERMAIN = ({ children, imagen, priority , info, }: PROPSHEADER) => {
 
     return (
         <header className="w-full  relative max-h-max overflow-hidden  ">
             <img
-                className="absolute inset-0 -z-10 w-full h-full object-cover brightness-[60%]"
+                className={`absolute inset-0 -z-10 w-full h-full object-cover brightness-[60%]`}
                 src={imagen}
                 alt={`Imagen de fondo de ${info}`}
                 loading={priority ? "eager" : "lazy"}
