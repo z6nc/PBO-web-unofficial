f98a2771c7ef6cd9b980dca1b3aaa05def45dd6a
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:39:18 -0500

    refactor: reorganize imports and remove unused Banner component from Home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,30 +1,27 @@
 import { Navbar } from "../components/layout/Navbar/navbar"
 import { HEADERMAIN, HeroSection } from "../components/layout/Header/index"
+import { LayoutPrincipal } from "../components/layout/LayoutPrincipal/layoutPrincipal";
 import { GridProgramacion, ListaCableOperadores } from "../components/layout/GridProgramaciones/index";
-import { Banner } from "../components/layout/Banner/banner"
 import { NewsletterSubscription } from "../components/common/Email/NewsletterSubscription";
 import { Footer } from "../components/layout/Footer/footer";
 import { ViewModalSocialMedia } from "../components/modal/ViewModalSocialMedia";
-import { EnVivoYoutubeBanner } from "../components/common/avisosBanner/enVivoYoutubeBanner";
+import { EnVivoYoutubeBanner, AlertaInformativa } from "../components/common/avisosBanner/index";
 export default function Home() {
     return (
-
         <div className="relative">
             <ViewModalSocialMedia />
-            <HEADERMAIN imagen="https://res.cloudinary.com/dleem7osr/image/upload/v1753805053/fondoPBO_ogifqw.png" info="PBO Radio" priority>
-                <Banner>
-                    <p className="text-white py-1">Este no es el sitio oficial de PBO </p>
-                </Banner>
+            <HEADERMAIN imagen="https://res.cloudinary.com/dleem7osr/image/upload/v1753805053/fondoPBO_ogifqw.png" info="PBO Radio"  priority>
+                <AlertaInformativa />
                 <Navbar />
                 <HeroSection />
             </HEADERMAIN>
-            <main className="w-full  lg:max-w-7xl mx-auto flex flex-col items-center justify-center gap-y-9 py-4">
+            <LayoutPrincipal>
                 <GridProgramacion rol="Conductor" numeroGRID={6} posiciontitulo="mr-auto" nombreTitulo="PROGRAMACION SEMANAL" />
-               <EnVivoYoutubeBanner />
+                <EnVivoYoutubeBanner />
                 <GridProgramacion rol="Periodista" numeroGRID={6} posiciontitulo="mr-auto" nombreTitulo="NUESTROS PRESENTADORES" />
                 <NewsletterSubscription />
                 <ListaCableOperadores categoria="cable" />
-            </main>
+            </LayoutPrincipal>
             <Footer />
         </div>
     )
