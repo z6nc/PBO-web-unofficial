0da944d575c25d60dc9ad6293b345b1f7e2ecf03
z6nc <andresvelasquezarotinco@gmail.com>
2025-07-31 17:38:44 -0500

    feat: add optional estilo prop to Modal component for customizable styling

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -2,12 +2,13 @@ import ReactDOM from 'react-dom';
 
 interface ModalProps {
     children: React.ReactNode;
+    estilo?: string;
 }
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children, estilo }: ModalProps) => {
 
     return ReactDOM.createPortal(
 
-        <div className='fixed inset-0 flex  justify-center items-center  z-50  '>
+        <div className={`fixed inset-0 flex  justify-center items-center  z-50  ${estilo}`}>
             {children}
         </div>,
         document.getElementById('modal')!
