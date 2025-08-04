2b3cea5e5760456abd1445ecd11964abca5273c8
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-04 12:53:02 -0500

    fix: add border and rounded corners to logo image for improved styling

--- src/components/common/LogoPBO/Logo.tsx
+++ src/components/common/LogoPBO/Logo.tsx
@@ -4,7 +4,7 @@ export const Logo = ({ estilo, priority=false }: LogoProps) => {
   return (
     <div className={estilo}>
       <img
-        className={`w-full h-full object-cover shadow-2xl`}
+        className={`w-full h-full object-cover shadow-2xl border border-white rounded-2xl`}
         src="https://pbo.pe/wp-content/uploads/2022/11/pbo.png"
         alt="Logo de PBO"
         loading={priority ? "eager" : "lazy"}
