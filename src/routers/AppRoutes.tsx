0edf3834bf4d2044dd14e29ddac9242c977e1890
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-04 12:54:39 -0500

    feat: import ProgramacionSemanal component and define route for weekly programming

--- src/routers/AppRoutes.tsx
+++ src/routers/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import NuestroEquipo from "../pages/nuestroEquipo";
+import ProgramacionSemanal from "../pages/programacionSemanal";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/nuestro-equipo" element={<NuestroEquipo />} />
-
+      <Route path="/programacion-semanal" element={<ProgramacionSemanal />} />
     </Routes>
   );
 }
