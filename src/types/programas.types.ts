cb2c1a0aa6227f9db82028406263ccd8e87083b7
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-04 12:54:47 -0500

    feat: add default option to Programa interface for better flexibility

--- src/types/programas.types.ts
+++ src/types/programas.types.ts
@@ -1,5 +1,5 @@
 export interface Programa {
-    programa: "Baella Talks" | "Agatha Lys en PBO" | "PBO con Chema Salcedo" | "PBO Noticias" | "PBO Marycarmen Sjoo" | "PBO Salud" | "PBO Campeonisimo" | "PBO Edicion Estelar" | "PBO con Rvk y Carmen" |"Rumbo Minero" |"PBO UMA Emprendedora"|"Vox Populi"|"Rescatando Valores"|"En la Mira"|"Conversando con el Perú"|"PBO con Denis Vargas Marín"|"Entre Nos"| "default";
+    programa: "Baella Talks" | "Agatha Lys en PBO" | "PBO con Chema Salcedo" | "PBO Noticias" | "PBO Marycarmen Sjoo" | "PBO Salud" | "PBO Campeonisimo" | "PBO Edicion Estelar" | "PBO con Rvk y Carmen" | "Rumbo Minero" | "PBO UMA Emprendedora" | "Vox Populi" | "Rescatando Valores" | "En la Mira" | "Conversando con el Perú" | "PBO con Denis Vargas Marín" | "Entre Nos" ;
     inicio: string;
     fin: string;
 }
@@ -8,3 +8,4 @@ export interface PROPSPROGRAMAS {
     dia: "Lunes" | "Martes" | "Miercoles" | "Jueves" | "Viernes" | "Sabado" | "Domingo";
     programas: Programa[];
 }
+
