24620a15ce21fb383884bee3c924348fb53192f0
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:31:28 -0500

    fix: add TipoNavbar prop to Navbar component for dynamic background styling

--- src/components/layout/Navbar/navbar.tsx
+++ src/components/layout/Navbar/navbar.tsx
@@ -3,13 +3,16 @@ import { Logo, ButtonCustom } from "../../common/index"
 import { ListaItems } from "./listaItem"
 import { MovilNavItem } from "./movilNavItem"
 import { HamburIcon, ExitIcon } from "../../icons/index"
-export const Navbar = () => {
+interface NavbarProps {
+  TipoNavbar: "md:bg-transparent" | "md:bg-black"
+}
+export const Navbar = ({ TipoNavbar }: NavbarProps) => {
   const [isOpen, setOpen] = useState<boolean>(false)
   const openClass = isOpen ? "hidden" : "block"
   const closeClass = isOpen ? "block" : "hidden"
   return (
     <>
-      <nav className="flex relative justify-around py-4  items-center  hover:backdrop-blur-md  bg-PBO lg:bg-transparent  ">
+      <nav className={`flex relative justify-around py-4  items-center  hover:backdrop-blur-md bg-black ${TipoNavbar}`}>
         <Logo estilo="w-20 lg:w-32 overflow-hidden" priority={true} />
         <ListaItems estilo="hidden lg:inline-flex text-white gap-x-9  " />
         <ButtonCustom onClick={() => setOpen(!isOpen)} estilo="block lg:hidden" ariaLabel="abrir menu movil" type="button">
@@ -23,3 +26,4 @@ export const Navbar = () => {
     </>
   )
 }
+// oklch(0.24 0 0)
