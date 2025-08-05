1b8fd31da17f7f366289ac58fa752c0bca6bdc85
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:30:24 -0500

    feat: enhance InputCustom component with register prop and improved styling

--- src/components/common/Email/inputCustom.tsx
+++ src/components/common/Email/inputCustom.tsx
@@ -1,11 +1,11 @@
 import type { InputCustomProps } from "../../../types/input.types"
-export const InputCustom = ({ type = "text", placeholder , error, ...rest }: InputCustomProps) => {
+export const InputCustom = ({ type = "text", placeholder, error, register, estilo, ...rest }: InputCustomProps) => {
     return (
-        <div>
-            <input className="w-72 md:w-96 h-16 bg-BlackPBO text-white placeholder:text-white px-4 rounded-md  focus:outline-none
-             focus:ring-2 focus:ring-white transition-colors" type={type} placeholder={placeholder}  {...rest} />
+        <div className="flex flex-col justify-center items-center ">
+            <input className={`${estilo}  bg-BlackPBO text-white placeholder:text-white px-4 py-2 rounded-md  focus:outline-none
+             focus:ring-2 focus:ring-white transition-colors `} type={type} placeholder={placeholder}  {...rest} {...register}  required/>
             {error && (
-                <span className="text-white text-sm mt-1">{error}</span>
+                <span className="text-PBO text-sm mt-1">{error}</span>
             )}
         </div>
     )
