bf75a8757587cfab178d5895b91317e4c5e5c56a
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:32:02 -0500

    fix: restructure ProgramacionSemanal component for improved layout and styling

--- src/pages/programacionSemanal.tsx
+++ src/pages/programacionSemanal.tsx
@@ -1,6 +1,7 @@
 
+import { HEADERMAIN } from "../components/layout/Header";
 import { Navbar } from "../components/layout/Navbar/navbar";
-import { SubtitleCustom } from "../components/common/titulos/index";
+import { Title,SubtitleCustom } from "../components/common/titulos/index";
 import { AlertaInformativa } from "../components/common/avisosBanner/alertaInformativa";
 import { LayoutPrincipal } from "../components/layout/LayoutPrincipal/layoutPrincipal";
 import { ListaProgramacion } from "../components/layout/ListaHorariosProgramacion/listaProgramacion";
@@ -9,8 +10,13 @@ import { Footer } from "../components/layout/Footer/footer";
 export default function ProgramacionSemanal() {
     return (
         <div className="relative">
-            <AlertaInformativa />
-            <Navbar />
+            <HEADERMAIN imagen="https://res.cloudinary.com/dleem7osr/image/upload/EquipoPBO_blqxgm.webp" info="PBO Radio" priority>
+                <AlertaInformativa />
+                <Navbar TipoNavbar="md:bg-transparent"/>
+                <section className=" h-[80dvh] text-center pt-9">
+                    <Title titulo="PROGRAMACION SEMANAL" />
+                </section>
+            </HEADERMAIN>
             <LayoutPrincipal>
                 <SubtitleCustom Subtitulo="Nuestra Programacion Semanal" estilo="text-center" />
                 <ListaProgramacion />
