664e642e04a0206a9fbd9cad1e2ea4fadeca96cb
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:31:22 -0500

    fix: update hover background color in ListaItems component for improved visibility

--- src/components/layout/Navbar/listaItem.tsx
+++ src/components/layout/Navbar/listaItem.tsx
@@ -7,7 +7,7 @@ export const ListaItems = ({ estilo }: { estilo: string }) => {
                 NavbarItems.map((itemNavbar, idx) => {
                     const importante = itemNavbar.isImportante ? "font-semibold  bg-red-600 text-white" : ""
                     return (
-                        <li className={`py-1 px-2 cursor-pointer  rounded-lg overflow-hidden ${importante} transition-colors duration-100 ease-linear hover:bg-PBO  `} key={idx}>
+                        <li className={`py-1 px-2 cursor-pointer  rounded-lg overflow-hidden ${importante} transition-colors duration-100 ease-linear hover:bg-PBO/80  `} key={idx}>
                             <Link to={itemNavbar.url}  >
                                 {itemNavbar.item}
                             </Link>
