843b4b961846da8128a87d2e066c75575155fbd3
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:32:19 -0500

    fix: add route for Publicitate page in AppRoutes

--- src/routers/AppRoutes.tsx
+++ src/routers/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import NuestroEquipo from "../pages/nuestroEquipo";
 import ProgramacionSemanal from "../pages/programacionSemanal";
+import Publicitate from "../pages/publicitate";
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/nuestro-equipo" element={<NuestroEquipo />} />
       <Route path="/programacion-semanal" element={<ProgramacionSemanal />} />
+      <Route path="/publicitate" element={<Publicitate />} />
     </Routes>
   );
 }
