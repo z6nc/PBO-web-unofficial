75cf26f359ce3b6c438ab5bd81699f1f01ec2983
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:30:07 -0500

    feat: add styling to AlertaInformativa banner for improved visibility

--- src/components/common/avisosBanner/alertaInformativa.tsx
+++ src/components/common/avisosBanner/alertaInformativa.tsx
@@ -2,8 +2,8 @@ import { Banner } from "../../layout/Banner/banner"
 
 export const AlertaInformativa = () => {
     return (
-        <Banner>
-            <p className="text-white py-1">Este no es el sitio oficial de PBO </p>
+        <Banner estilo="border-b border-red-500">
+            <p className="text-white py-1 ">Este no es el sitio oficial de PBO </p>
         </Banner>
     )
 }
