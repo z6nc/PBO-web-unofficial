be18fd0aaee2e9fb250990c44e14b474d73099c0
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:32:12 -0500

    feat: add Publicitate page with layout and components

---
+++ src/pages/publicitate.tsx
@@ -0,0 +1,26 @@
+
+import { Navbar } from "../components/layout/Navbar/navbar";
+import { SubtitleCustom } from "../components/common/titulos/index";
+import { AlertaInformativa } from "../components/common/avisosBanner/alertaInformativa";
+import { LayoutPrincipal } from "../components/layout/LayoutPrincipal/layoutPrincipal";
+import { FormPublicitate } from "../components/layout/Formularios/formPublicitate";
+import { Footer } from "../components/layout/Footer/footer";
+export default function Publicitate() {
+    return (
+        <div className="relative">
+                <AlertaInformativa />
+                <Navbar TipoNavbar="md:bg-black" />
+               
+            <LayoutPrincipal>
+                <SubtitleCustom Subtitulo="Forma parte de nuestra familia" estilo="text-center" />
+                <section className="grid grid-cols-1  md:grid-cols-2 gap-4 justify-center mt-9 lg:mb-24">
+                    <FormPublicitate />
+                    <div className=" w-full h-full overflow-hidden ">
+                        <img loading="lazy" className="w-full object-cover h-full " src="https://res.cloudinary.com/dleem7osr/image/upload/setPBO_ympdqa.webp" alt="Set de PBO" />
+                    </div>
+                </section>
+            </LayoutPrincipal>
+            <Footer />
+        </div>
+    );
+}
