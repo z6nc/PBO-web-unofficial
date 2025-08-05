438db6959229d56742bab877606016942fcb6d6d
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:30:32 -0500

    feat: update NewsletterSubscription component styling for improved layout

--- src/components/common/Email/NewsletterSubscription.tsx
+++ src/components/common/Email/NewsletterSubscription.tsx
@@ -4,11 +4,11 @@ import { ButtonCustom } from "../buttom/buttonCustom";
 import { DescriptionCustom } from "../titulos/descriptionCustom";
 export function  NewsletterSubscription() {
     return (
-        <Banner estilo="py-36">
+        <Banner estilo=" h-screen">
             <h3 className="font-Monserat text-2xl  md:text-3xl 2xl:text-4xl text-white font-bold py-2">Sé el primero en saberlo.</h3>
             <DescriptionCustom descripcion="Únete a nuestro newsletter." />
             <form action="" className="flex flex-col md:flex-row items-center justify-center gap-2 pt-4" >
-                <InputCustom type="email" placeholder="Tu Email" required/>
+                <InputCustom estilo=" w-72 md:w-96 h-16" type="email" placeholder="Tu Email" required/>
                 <ButtonCustom ariaLabel="enviar el correo" type="submit" estilo="border border-white bg-black py-4 px-6 text-white active:scale-95 transition-all duration-300">
                     Enviar
                 </ButtonCustom>
