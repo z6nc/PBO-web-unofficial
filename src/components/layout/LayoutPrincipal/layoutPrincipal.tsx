eadb1544cf3b25bcc2e09f97db4d4472a82f6bbb
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:31:16 -0500

    fix: remove padding from LayoutPrincipal component for improved layout consistency

--- src/components/layout/LayoutPrincipal/layoutPrincipal.tsx
+++ src/components/layout/LayoutPrincipal/layoutPrincipal.tsx
@@ -1,6 +1,6 @@
 export function LayoutPrincipal({ children }: { children: React.ReactNode }) {
     return (
-      <main className="w-full  lg:max-w-7xl mx-auto flex flex-col items-center justify-center gap-y-2 py-4">
+      <main className="w-full  lg:max-w-7xl mx-auto flex flex-col items-center justify-center gap-y-2 ">
         {children}
       </main>
     );
