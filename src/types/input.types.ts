2b87d606de846836beacd85ee6b81e47d98e6e76
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 12:32:32 -0500

    fix: update InputCustomProps interface to include UseFormRegisterReturn type

--- src/types/input.types.ts
+++ src/types/input.types.ts
@@ -1,7 +1,9 @@
 import type { InputHTMLAttributes } from "react";
+import type { UseFormRegisterReturn } from "react-hook-form";
 
-export interface InputCustomProps extends InputHTMLAttributes<HTMLInputElement>  {
+export interface InputCustomProps extends InputHTMLAttributes<HTMLInputElement> {
     placeholder: string;
     error?: string
-
+    register?: UseFormRegisterReturn,
+    estilo? :string
 }
