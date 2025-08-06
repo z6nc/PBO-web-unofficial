6f4d8e8b8b7a8e0266c724bcf54a6f5275855aa5
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-06 16:24:31 -0500

    feat: add MapaPbo component to display an embedded Google Map

---
+++ src/components/layout/MapaPbo/mapaPbo.tsx
@@ -0,0 +1,7 @@
+export function MapaPbo() {
+    return (
+          <div className=" h-[60dvh] lg:w-[60dvh] lg:h-[50dvh] overflow-hidden ">
+              <iframe className="w-full h-full object-cover"  src="https://maps.google.com/maps?t=m&amp;output=embed&amp;iwloc=near&amp;z=15&amp;q=Av.+Angamos+Oeste+651%2C+Miraflores+15074%2C+Per%C3%BA" scrolling="no" frameBorder="0" aria-label="One" ></iframe>
+          </div>
+        );
+}
