c05fbe5974b86c2c8ad116e2f042633d0b54d447
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-06 16:25:02 -0500

    feat: add route for Contactos page in AppRoutes

--- src/routers/AppRoutes.tsx
+++ src/routers/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Home from "../pages/home";
 import NuestroEquipo from "../pages/nuestroEquipo";
 import ProgramacionSemanal from "../pages/programacionSemanal";
 import Publicitate from "../pages/publicitate";
+import Contactos from "../pages/contactos";
 
 export default function AppRoutes() {
   return (
@@ -11,6 +12,8 @@ export default function AppRoutes() {
       <Route path="/nuestro-equipo" element={<NuestroEquipo />} />
       <Route path="/programacion-semanal" element={<ProgramacionSemanal />} />
       <Route path="/publicitate" element={<Publicitate />} />
+      <Route path="/contacto" element={<Contactos />} />
+
     </Routes>
   );
 }
