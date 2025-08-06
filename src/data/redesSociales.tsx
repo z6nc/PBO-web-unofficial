921abe8abc5dda5eafc656b4b20b18502bec665e
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-06 16:24:48 -0500

    refactor: update social media icons to use lucide-react components and clean up code

--- src/data/redesSociales.tsx
+++ src/data/redesSociales.tsx
@@ -1,35 +1,35 @@
-import { TiktokIcon, FacebookIcon, IstagramIcon, XIcon, YoutubeIcon ,WhatsApp} from "../components/icons/redesSocialesIcons"
+import { Facebook, Twitter, Instagram, Youtube, Phone } from 'lucide-react';
 import type { PROPSREDESSOCIALES } from "../types/RedesSociales.types"
 export const RedesSocialesPBO: PROPSREDESSOCIALES[] = [
   {
     NombreRedSocial: "Youtube",
     url: "https://www.youtube.com/@PBO",
-    logo: <YoutubeIcon estilo="size-5 lg:size-7" />,
+    logo: <Youtube  className='h-5 w-5'/>,
   },
   {
     NombreRedSocial: "Facebook",
     url: "https://www.facebook.com/PBOPeru",
-    logo: <FacebookIcon estilo="size-5 lg:size-7" />
+    logo: <Facebook className='h-5 w-5' />
   },
   {
     NombreRedSocial: "X",
     url: "https://x.com/PBOPeru",
-    logo: <XIcon estilo="size-5 lg:size-7" />
+    logo: <Twitter className='h-5 w-5' />
   },
   {
     NombreRedSocial: "Instagram",
     url: "https://www.instagram.com/pbo_digital/#",
-    logo: <IstagramIcon estilo="size-5 lg:size-7" />
+    logo: <Instagram className='h-5 w-5' />
   },
-  {
-    NombreRedSocial: "Tik Tok",
-    url: "https://www.tiktok.com/@pbo_digital",
-    logo: <TiktokIcon estilo="size-5 lg:size-7"  />
-  },
+  // {
+  //   NombreRedSocial: "Tik Tok",
+  //   url: "https://www.tiktok.com/@pbo_digital",
+  //   logo: <TiktokIcon estilo="size-5 lg:size-7"  />
+  // },
   {
     NombreRedSocial: "WhatsApp",
     url: "https://api.whatsapp.com/send/?phone=5116449550&text&type=phone_number&app_absent=0",
-    logo: <WhatsApp estilo="size-6 lg:size-7" />,
+    logo: <Phone className='h-5 w-5' />,
     info: "+51 1 6449550"
   }
 
