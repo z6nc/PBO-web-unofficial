a79b6a3bed0b0e7d6980dd2fc313b4382006128a
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-06 16:23:01 -0500

    fix: adjust styling and layout in SectionContactos component

--- src/components/common/contactos/contactos.tsx
+++ src/components/common/contactos/contactos.tsx
@@ -6,10 +6,10 @@ export function SectionContactos({ estilo }: { estilo?: string }) {
     const redesSociales = RedesSocialesPBO.find(item => item.NombreRedSocial === "WhatsApp");
 
     return (
-        <div className={`${estilo} flex flex-col gap-y-9 lg:gap-y-5 text-white py-2`}>
+        <div className={`${estilo} flex  gap-y-9 lg:gap-y-5  `}>
             <div className="flex  flex-col text-center  lg:text-start lg:flex-row items-center gap-2">
-                <PinMap estilo="size-15 lg:size-9" />
-                <div className="flex flex-col">
+                <PinMap estilo="size-13 " />
+                <div className="flex flex-col text-wrap w-72 ">
                     <span className="font-bold">Oficina PBO</span>
                     <span>Av. Angamos Oeste 651, Miraflores, Perú</span>
                 </div>
@@ -19,9 +19,12 @@ export function SectionContactos({ estilo }: { estilo?: string }) {
                 redesSociales ? (
                     <a key={redesSociales.NombreRedSocial} href={redesSociales.url} target="_blank" rel="noopener noreferrer" className="
                      flex  flex-col text-center  lg:text-start lg:flex-row items-center gap-2 pl-1">
-                        <WhatsApp estilo="size-10 lg:size-8" />
-                        <span className="font-bold">{redesSociales.NombreRedSocial}</span>
-                        <span>{redesSociales.info}</span>
+                        <WhatsApp estilo="size-10 " />
+                        <div className="flex flex-col text-wrap w-72">
+                            <span className="font-bold">{redesSociales.NombreRedSocial}</span>
+                            <span>{redesSociales.info}</span>
+
+                        </div>
                     </a>
                 ) : null
             }
