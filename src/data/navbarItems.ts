f3bdb9000e21013bca8c754b400d864e0f0d5880
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-06 16:24:38 -0500

    refactor: clean up NavbarItems by removing unused "En Vivo" entry and formatting

--- src/data/navbarItems.ts
+++ src/data/navbarItems.ts
@@ -1,28 +1,29 @@
 import type { PROPSNAV } from "../types/navbar.types"
-export const NavbarItems :PROPSNAV[] = [
+export const NavbarItems: PROPSNAV[] = [
     {
         item: "Inicio",
         url: "/",
         isImportante: false
     },
-    {
-        item: "En Vivo ",
-        url: "/",
-        isImportante: true
-    },
+   
     {
         item: "Programas",
         url: "/programacion-semanal",
         isImportante: false
     },
-      {
+    {
         item: "Nuestro equipo",
         url: "/nuestro-equipo",
         isImportante: false
     },
-        {
+    {
         item: "Publicitate",
         url: "/publicitate",
+        isImportante: false
+    },
+     {
+        item: "Contacto",
+        url: "/contacto",
         isImportante: false
     },
 ]
