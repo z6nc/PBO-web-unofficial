a104a24c07e238b07bf6a905f242dfe871c1edea
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 08:37:18 -0500

    refactor: enhance styling for Title component for improved consistency

--- src/components/common/titulos/titleCustom.tsx
+++ src/components/common/titulos/titleCustom.tsx
@@ -1,5 +1,5 @@
 export const Title = ({ titulo }: { titulo: string }) => {
     return (
-        <h1 className="font-Monserat uppercase text-center text-white text-4xl md:text-5xl 2xl:text-7xl font-black drop-shadow-md drop-shadow-black">{titulo}</h1>
+        <h1 className="font-Monserat text-wrap uppercase text-center text-white text-4xl md:text-5xl 2xl:text-6xl font-black drop-shadow-md ">{titulo}</h1>
     )
 }
