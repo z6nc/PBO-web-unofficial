d55ea10fa178552091eb7a58606be049f026dea2
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 08:37:37 -0500

    refactor: enhance layout and styling for GridProgramacion component for improved consistency

--- src/components/layout/GridProgramaciones/gridProgramacion.tsx
+++ src/components/layout/GridProgramaciones/gridProgramacion.tsx
@@ -7,7 +7,7 @@ export const GridProgramacion = ({ rol, numeroGRID, posiciontitulo, nombreTitulo
     const filteredColaboradores = rol ? colaboradores.filter(colaborador => colaborador.rol === rol) : colaboradores;
 
     return (
-        <section className="flex flex-col mx-4 lg:mx-0">
+        <section className="flex flex-col mx-4 lg:mx-0 justify-center items-center">
             <SubtitleCustom Subtitulo={nombreTitulo} estilo={posiciontitulo} />
             <div className="grid grid-cols-1 grid-rows-3  md:grid-cols-2  lg:grid-cols-6   gap-4 overflow-hidden lg:h-[900px]">
                 {
@@ -15,8 +15,8 @@ export const GridProgramacion = ({ rol, numeroGRID, posiciontitulo, nombreTitulo
                         <div key={index} className={`relative ${colaborador.estilo} rounded-lg overflow-hidden group `}>
                             <img loading="lazy" className=" w-full h-full object-cover object-top group-hover:scale-110 transition-all ease-linear duration-150 " src={colaborador.imagen as string} alt={colaborador.NombreCompleto} />
                             <div className="absolute flex flex-col  z-20 text-white bottom-0 w-60 ">
-                                <span className="bg-PBO/60 py-2 group-hover:bg-PBO/80 px-4 mr-auto">{colaborador.horarios ?? colaborador.rol}</span>
-                                <h4 className=" bg-BlackPBO p-2 font-semibold text-lg font-Monserat ">{colaborador.programa ?? colaborador.NombreCompleto}</h4>
+                                <span className="bg-PBO/60 py-2 text-sm md:text-base  group-hover:bg-PBO/80 px-4 mr-auto">{colaborador.horarios ?? colaborador.rol}</span>
+                                <h4 className=" bg-BlackPBO p-2 font-semibold text-base md:text-lg font-Monserat ">{colaborador.programa ?? colaborador.NombreCompleto}</h4>
                             </div>
                         </div>
                     ))
