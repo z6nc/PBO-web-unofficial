99aad63bb6c826336818f217677dbeb6d778fe8a
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 10:47:07 -0500

    refactor: remove unnecessary margin from SubtitleCustom component

--- src/components/common/titulos/subtitleCustom.tsx
+++ src/components/common/titulos/subtitleCustom.tsx
@@ -1,7 +1,7 @@
 
 export const SubtitleCustom = ({ Subtitulo, estilo }: { Subtitulo: string, estilo?: string }) => {
     return (
-        <div className={`bg-PBO text-white py-4 px-4  my-7 ${estilo}`}>
+        <div className={`bg-PBO text-white py-4 px-4   ${estilo}`}>
             <h2 className="text-2xl md:text-3xl font-bold  font-Monserat uppercase ">{Subtitulo}</h2>
         </div>
     );
