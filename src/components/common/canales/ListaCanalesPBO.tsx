5254063e0f3603b591bfe838414fa874a410e456
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 08:36:41 -0500

    refactor: improve styling for ListaCanalesPbo component for better responsiveness

--- src/components/common/canales/ListaCanalesPBO.tsx
+++ src/components/common/canales/ListaCanalesPBO.tsx
@@ -2,8 +2,8 @@ import { canalesPBO } from "../../../data/canalesPBO"
 export const ListaCanalesPbo = () => {
     return (
         <section className=" my-9">
-            <p className="text-lg text-white py-2 rounded-lg">Más cerca de ti, por aire, cable e internet</p>
-            <div className="flex flex-wrap justify-center items-center gap-4">
+            <p className="text-baselg:text-lg text-white py-2 rounded-lg">Más cerca de ti, por aire, cable e internet</p>
+            <div className="flex flex-wrap justify-center items-center gap-4 mx-4 lg:mx-0 ">
                 {
                     canalesPBO.slice(0, 3).map((canal, idx) => (
                         <a key={idx} href={canal.url} target="_blank" className=" inline-flex items-center gap-4 text-gray-200 border border-gray-300 rounded-lg backdrop-blur-lg py-1 px-2">
