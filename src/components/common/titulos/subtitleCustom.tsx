60f060a1e7ca93e0009f617f97f47f33634ca96a
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 08:37:11 -0500

    refactor: update styling for SubtitleCustom component for improved consistency

--- src/components/common/titulos/subtitleCustom.tsx
+++ src/components/common/titulos/subtitleCustom.tsx
@@ -2,7 +2,7 @@
 export const SubtitleCustom = ({ Subtitulo, estilo }: { Subtitulo: string, estilo?: string }) => {
     return (
         <div className={`bg-PBO text-white py-4 px-4  my-7 ${estilo}`}>
-            <h2 className="text-3xl font-bold  font-Monserat uppercase ">{Subtitulo}</h2>
+            <h2 className="text-2xl md:text-3xl font-bold  font-Monserat uppercase ">{Subtitulo}</h2>
         </div>
     );
 }
