c541dabb79e23786a2d44f3d9ac13687ce484d01
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 08:36:21 -0500

    refactor: update EnVivoYoutubeBanner heading styles for improved responsiveness

--- src/components/common/avisosBanner/enVivoYoutubeBanner.tsx
+++ src/components/common/avisosBanner/enVivoYoutubeBanner.tsx
@@ -3,7 +3,7 @@ import { DescriptionCustom } from "../titulos/descriptionCustom";
 export const EnVivoYoutubeBanner = () => {
     return (
         <Banner estilo="text-center h-screen">
-            <h3 className="font-Monserat text-2xl  md:text-3xl 2xl:text-4xl text-white font-bold py-2">¡Estamos en Vivo en YouTube!</h3>
+            <h3 className="font-Monserat text-wrap text-3xl 2xl:text-4xl text-white font-bold py-2">¡Estamos en Vivo en YouTube!</h3>
             <DescriptionCustom descripcion="Únete a nuestra transmisión en vivo y no te pierdas ningún detalle." />
             <a href="https://www.youtube.com/@pbo" target="_blank" className="cursor-pointer bg-black border-white border  rounded transition-all ease-linear duration-100 hover:scale-105 p-4 my-4">
                 <p className="  text-white  ">Ver en YouTube</p>
