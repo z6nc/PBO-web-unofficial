efe1cd7e2b87ebda85b9b52e48f5a67f0232e1ea
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 08:37:03 -0500

    refactor: adjust text size and styling for DescriptionCustom component for consistency

--- src/components/common/titulos/descriptionCustom.tsx
+++ src/components/common/titulos/descriptionCustom.tsx
@@ -1,6 +1,6 @@
 
 export const DescriptionCustom = ({ descripcion , estilo }:{descripcion :string , estilo?:string}) => {
     return (
-        <p className={` text-white text-xl md:text-lg 2xl:text-xl font-bold ${estilo}`}>{descripcion}</p>
+        <p className={` text-white text-lg md:text-xl  ${estilo}`}>{descripcion}</p>
     )
 }
