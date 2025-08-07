038f504b19160544c8544818da216bc6101ba71f
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-07 11:19:32 -0500

    refactor: restructure RedesSociales component for improved layout and readability

--- src/components/common/RedesSociales/listRedsocial.tsx
+++ src/components/common/RedesSociales/listRedsocial.tsx
@@ -1,18 +1,22 @@
 import { RedesSocialesPBO } from "../../../data/redesSociales";
 export function RedesSociales({ estilo }: { estilo?: string }) {
   return (
-    <div className={`flex space-x-3 ${estilo}`}>
-      {RedesSocialesPBO.map((red) => (
-        <a
-          key={red.NombreRedSocial}
-          href={red.url}
-          target="_blank"
-          className="group relative p-3 bg-white/5 hover:bg-white/15 rounded-xl transition-all duration-300 border border-white/10 hover:border-white/30 hover:scale-110"
-          aria-label={red.NombreRedSocial}
-        >
-          {red.logo}
-        </a>
-      ))}
+    <div className="space-y-4">
+      <h4 className="text-sm font-semibold text-white">Síguenos</h4>
+      <div className={`flex space-x-3 ${estilo}`}>
+        {RedesSocialesPBO.map((red) => (
+          <a
+            key={red.NombreRedSocial}
+            href={red.url}
+            target="_blank"
+            className="group relative p-3  bg-black/90 hover:bg-white hover:text-black rounded-xl transition-all duration-300 border border-white hover:border-white/30 hover:scale-110"
+            aria-label={red.NombreRedSocial}
+          >
+            {red.logo}
+          </a>
+        ))}
+      </div>
     </div>
+
   );
 }
